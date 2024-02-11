86db7040095bcb09d1211b15d6809034380e51b1
Shiven Sheth <105996940+shiventi@users.noreply.github.com>
2024-02-10 23:03:35 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@ const toggleMenu = (toggleId, navId) => {
     }
 };
 
+
+
 toggleMenu('nav-toggle', 'nav-menu');
 
 const navLinks = document.querySelectorAll('.nav__link');
@@ -17,6 +19,36 @@ function handleLinkAction() {
     const navigationMenu = document.getElementById('nav-menu');
     navigationMenu.classList.remove('show');
 }
+
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// Function to create a star
+function createStar() {
+  const star = document.createElement('div');
+  star.classList.add('star');
+  const size = random(1, 3) + 'px';
+  star.style.width = size;
+  star.style.height = size;
+  star.style.top = random(0, 100) + '%';
+  star.style.left = random(0, 100) + '%';
+  const duration = random(5, 15) + 's';
+  star.style.animation = `twinkle ${duration} linear infinite`;
+  document.querySelector('.stars').appendChild(star);
+}
+
+// Create stars
+for (let i = 0; i < 300; i++) {
+  createStar();
+}
+
+const toggleBtn = document.querySelector('.dark-mode-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
 
 navLinks.forEach(link => link.addEventListener('click', handleLinkAction));
 
